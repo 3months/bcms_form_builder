b26cb1de807d62fc29a0271030880850d8068636
Ken Chirsden <ken@nermal.3months.com>
2010-01-15 09:16:16 +1300

    Adding support for radio buttons

---
+++ app/models/radio_button_custom_form_element.rb
@@ -0,0 +1,29 @@
+class RadioButtonCustomFormElement < CustomFormElement
+
+  @@radio_button_config = nil
+
+  def self.config
+    return @@radio_button_config unless @@radio_button_config.nil?
+
+    @@radio_button_config = CustomFormElementConfig.new(
+      :radio_button, 'Radio Button Input',
+      :collecting => true,
+      :accessors => {:label => :direct_value, :options => :radio_button_options, :classes => :class_values},
+      :readers => {:options_array => [:options, :radio_button_options_array], :class_array => [:classes, :class_array]}
+    )
+    @@radio_button_config.freeze
+
+    return @@radio_button_config
+  end
+
+  # self.print_value
+  #
+  # Should be a Hash.  If not just #inspect.  If Hash, render as a list of keys.
+  #
+  def self.print_value(value)
+    return value.inspect unless value.is_a?(Hash)
+
+    return value.keys.map {|k| k.inspect}.join(', ')
+  end
+
+end
