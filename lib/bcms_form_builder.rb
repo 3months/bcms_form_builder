51eddbf4e34d1858622414598a17ad6e8e308cc2
Kim Chirnside <kim@dashy.(none)>
2010-02-28 16:39:13 +1300

    Correcting hide/show issue with radio button options

--- lib/bcms_form_builder.rb
+++ lib/bcms_form_builder.rb
@@ -2,3 +2,5 @@ require 'bcms_form_builder/routes'
 require 'bcms_form_builder/helpers.rb'
 require 'bcms_form_builder/custom_form_builder.rb'
 
+ActionView::Base.default_form_builder = Cms::CustomFormBuilder
+
